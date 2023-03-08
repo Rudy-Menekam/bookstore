17421ef499d78e74d57e309839c061f15de8ec91
Menekam Rudy <menekamrudy@gmail.com>
2023-03-08 23:58:04 +0100

    Change use booksReducer rather than booksSlice and verse versa

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { bookSlice } from './books/booksSlice';
-import { categoriesSlice } from './categories/categoriesSlice';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
-    Book: bookSlice,
-    categories: categoriesSlice,
+    Book: booksReducer,
+    categories: categoriesReducer,
   },
 });
 export default store;
